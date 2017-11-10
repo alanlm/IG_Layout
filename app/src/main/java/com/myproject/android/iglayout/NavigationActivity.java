8b07d9fd4781d0730902ee1b52fde8b1207d0a76
Alan Manalang <a_rabbit93@yahoo.com>
2017-11-10 15:39:13 -0800

    changed base activity name to navigation activity

--- app/src/main/java/com/myproject/android/iglayout/NavigationActivity.java
+++ app/src/main/java/com/myproject/android/iglayout/NavigationActivity.java
@@ -3,11 +3,11 @@ package com.myproject.android.iglayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BaseActivity extends AppCompatActivity {
+public class NavigationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+        setContentView(R.layout.activity_navigation);
     }
 }
